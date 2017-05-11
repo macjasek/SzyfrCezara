64c49ff49e1def392d2b87450d9883eda0484bb5
macjasek <maciej.dobruchowski@ispro.com.pl>
2017-05-11 18:16:37 +0200

    To GitHub

---
+++ SzyfrCezara/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzyfrCezara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj słowo do zaszyfrowania");
+            string slowo = Console.ReadLine();
+
+            string slowoSzyfr = "";
+            const int SHIFT = 3;
+            int ascIndex = 0;
+
+            foreach (char letter in slowo)
+            {
+                ascIndex = (int)letter;
+
+                if (char.IsLower (letter) )
+                {
+                    ascIndex = (ascIndex < 120) ? ascIndex += SHIFT : ascIndex += SHIFT - 26;
+                }
+                else if (char.IsUpper(letter))
+                {
+                    ascIndex = (ascIndex < 88) ? ascIndex += SHIFT : ascIndex += SHIFT - 26;
+                }
+                Console.WriteLine(ascIndex);
+                slowoSzyfr += (char)ascIndex;
+            }
+
+            Console.WriteLine(slowoSzyfr);
+        }
+    }
+}
